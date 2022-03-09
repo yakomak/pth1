733de7780f6f9675dd0e21469a17d2dc4c193f89
Maxim <yakomak137@gmail.com>
2022-03-09 14:02:04 +0300

    Create py3.py
    new

---
+++ py3.py
@@ -0,0 +1,10 @@
+import time
+startTime = time.time()
+for i in range(0,5):
+    print(i) # making delay for 1 second
+    time.sleep(1)
+endTime = time.time()
+elapsedTime = endTime - startTime
+print("Elapsed Time = %s" % elapsedTime)
+
+# Источник: https://pythonim.ru/chisla/funktsiya-time-sleep-v-python
